e22cd637b7716be47269543fed98f1e5d54b7b80
baothi <baothi246@gmail.com>
2019-12-03 04:58:06 -0800

    update contact

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    get 'contacts/index', as: 'contacts'
+   get 'contacts/new', as: 'new_contact'
+   post 'contacts/create', as: 'create_contact'
+   get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
+   patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
    root 'contacts#index'
 end
